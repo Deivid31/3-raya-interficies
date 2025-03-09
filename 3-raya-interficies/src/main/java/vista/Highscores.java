63e9ff1ca53100d5575158b2b15d006624c5ae18
David Romero <david.ropi.2005@gmail.com>
2025-03-09 03:07:37 +0100

    Solucionado pequeño error

--- 3-raya-interficies/src/main/java/vista/Highscores.java
+++ 3-raya-interficies/src/main/java/vista/Highscores.java
@@ -35,7 +35,6 @@ public class Highscores extends javax.swing.JFrame {
         this.user = user;
         this.translationService = translationService;
         gameDAO = new GameDAO();
-        translatePage();
         ImageIcon img = new ImageIcon("src\\main\\java\\images\\icon.png");
         setIconImage(img.getImage());
         jTaLeaderboard.setModel(new LeaderboardTableModel(gameDAO.getUsers()));
